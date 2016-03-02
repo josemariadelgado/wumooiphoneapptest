7feac512f741ecfca719b77dd5b07ccff6622c94
José María Delgado <josemaria@mbp-de-jose.local.tuguu.com>
2016-03-02 12:04:55 +0000

    Initial Commit

---
+++ wumoo-test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  wumoo-test
+//
+//  Created by José María Delgado  on 2/3/16.
+//  Copyright © 2016 José María Delgado . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
